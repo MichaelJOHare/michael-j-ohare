b45fd0dd59c679ca96becd228f77683db1fe1d33
MichaelJOHare <mjohare13@gmail.com>
2024-02-18 01:49:54 -0500

    Updated main.js TODO list

--- public/chess/main.js
+++ public/chess/main.js
@@ -7,15 +7,12 @@ TO DO
  - Fix best move arrow drawn after move during analysis (replace if lastCommand === "stop" conditional)
               - fix best move arrow removed when switching between analysis types
                      - FIX stockfish initialized twice switching analysis types
- - FIX commands sent to stockfish before readyok is received (getStockfishAsOpponentMove)
-        --Implement stockfish castling
- - Add current selection highlight to modal!!!
+ -Finish implementing stockfish castling/promotion/en passant
  - Implement captured pieces/material advantage
+
  - Fix king check highlight after promotion after reset game ??
-    - Fix king check highlight after stockfish move
 
  - Show current depth/sf advantage in analysis mode
- - Refactor/clean up SFController, kind of hacky right now
 
  - Make drawing arrow/circle over pre-existing arrow/circle remove it.
 
@@ -23,8 +20,7 @@ TO DO
      -Thought it was a good idea, not liking it now.  Maybe keep setters?  They make sense in my brain
  - Make move piece with click a smooth glide to location
       - Add sounds
- - Implement play vs stockfish.  Modal, if play vs -> select ELO or skill level.  Remove import from FEN/SF analysis
-        - If player selects black, auto flip board
+ - Implement if play vs stockfish -> if player selects black, auto flip board
  
  - Implement PGN box
 */
